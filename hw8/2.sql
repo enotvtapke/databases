5b3afb598b010abf3c4ddb784bd53d1c26fcb078
Aleksandr Stupnikov <a.stupnikov05@gmail.com>
2023-11-13 09:52:29 +0300

    Split long comments on separate lines

--- hw8/2.sql
+++ hw8/2.sql
@@ -1,9 +1,12 @@
 select avg(cast(Mark as float)) from students natural join marks natural join groups where groupname = :GroupName;
 
--- Индекс поможет быстро найти группу с заданным названием. Так как groupName сильно больше, чем groupId, стоит сделать индекс покрывающим.
+-- Индекс поможет быстро найти группу с заданным названием. Так как
+-- groupName сильно больше, чем groupId, стоит сделать индекс покрывающим.
 -- Так не будет обращения в таблицу groups.
 create unique index groups_groupName on groups using btree (groupname, groupid);
 
--- Индекс поможет сделать join таблиц students и marks по studentId. Использован покрывающий индекс, чтобы не делать лишний запрос к marks.
--- Это спорное решение, которое было сделано из предположения, что mark с большой вероятностью занимает мало места.
+-- Индекс поможет сделать join таблиц students и marks по studentId.
+-- Использован покрывающий индекс, чтобы не делать лишний запрос к marks.
+-- Это спорное решение, которое было сделано из предположения, что mark
+-- с большой вероятностью занимает мало места.
 create index marks_studentId_mark on marks using btree (studentid, mark);
--- hw8/3.sql
+++ hw8/3.sql
@@ -1,11 +1,13 @@
 -- Список студентов группы, отсортированный по имени студента
 select studentname from students where groupid = :GroupId order by studentname;
--- Нужна возможность сортировки => btree. Чтобы не сортировать всех, а только студентов в конкретной группе, первый столбец индекс это groupId
+-- Нужна возможность сортировки => btree. Чтобы не сортировать всех,
+-- а только студентов в конкретной группе, первый столбец индекс это groupId
 create index students_groupId_studentName on students using btree (groupid, studentname);
 
 -- Максимальная оценка по курсу среди всех студентов
 select max(mark) from marks where courseid = :CourseId;
--- Чтобы искать максимум только в рамках заданного курса, но при этом среди всех студентов, первый столбец courseId. btree потому что max
+-- Чтобы искать максимум только в рамках заданного курса, но при этом
+-- среди всех студентов, первый столбец courseId. btree потому что max
 create index marks_courseId_mark on marks using btree (courseid, mark);
 
 -- Все курсы, которые ведёт лектор у группы
