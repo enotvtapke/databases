5cc89daa45feeb330e3129b18949d53bebace8e9
Aleksandr Stupnikov <a.stupnikov05@gmail.com>
2023-11-17 10:34:43 +0300

    Fix task 2 from hw8

--- hw8/2.sql
+++ hw8/2.sql
@@ -1,9 +1,19 @@
-select avg(cast(Mark as float)) from students natural join marks natural join groups where groupname = :GroupName;
+select avg(cast(Mark as float))
+from students
+         natural join groups
+         natural join marks
+         natural join courses
+where groupname = :GroupName
+  and coursename = :CourseName;
 
 -- Индекс поможет быстро найти группу с заданным названием. Так как
 -- groupName сильно больше, чем groupId, стоит сделать индекс покрывающим.
 -- Так не будет обращения в таблицу groups.
 create unique index groups_groupName on groups using btree (groupname, groupid);
+
+-- Индекс поможет быстро найти курс с заданным названием. Так как
+-- courseName сильно больше, чем groupId, стоит сделать индекс покрывающим.
+create unique index courses_courseName_courseId on courses using btree (coursename, courseid);
 
 -- Индекс поможет сделать join таблиц students и marks по studentId.
 -- Использован покрывающий индекс, чтобы не делать лишний запрос к marks.
